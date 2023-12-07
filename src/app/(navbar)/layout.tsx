73d059d553573512e83063d61b48d424089207f0
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2023-12-07 16:09:32 +0700

    feat: schedule page

--- src/app/(navbar)/layout.tsx
+++ src/app/(navbar)/layout.tsx
@@ -2,8 +2,10 @@ import Navbar from "../components/navbar";
 
 const NavbarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-full w-full flex flex-col relative">
-      <div className="flex-1 bg-[#F5F6F7] pb-[4.2rem]">{children}</div>
+    <div className="h-full w-full flex flex-col relative">
+      <div className="flex-1 bg-[#F5F6F7] overflow-y-auto">
+        <div className="px-[5%] pt-10">{children}</div>
+      </div>
       <Navbar />
     </div>
   );
--- src/app/(navbar)/schedule/page.tsx
+++ src/app/(navbar)/schedule/page.tsx
@@ -1,5 +1,37 @@
+import ScheduleCard from "./components/schedule-card";
+
+const scheduleData = [
+  {
+    type: "Private Space",
+    name: "Room A",
+    date: "23 Desember 2023",
+    time: "12:00 PM - 3:00PM",
+  },
+  {
+    type: "Open Space",
+    name: "Open Space A",
+    date: "23 Desember 2023",
+    time: "12:00 PM - 3:00PM",
+  },
+  {
+    type: "Event",
+    name: "Artificial Intelligence Seminar",
+    date: "23 Desember 2023",
+    time: "12:00 PM - 3:00PM",
+  },
+];
+
 const Page = () => {
-  return ( <div></div> );
-}
- 
+  return (
+    <div className="w-full overflow-y-auto h-full">
+      <h1 className="font-semibold text-lg mb-5">Your Booking Schedule</h1>
+      <div className="w-full flex flex-col gap-4 pb-5">
+        {scheduleData.map((data, index) => (
+          <ScheduleCard key={index} {...data} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Page;
