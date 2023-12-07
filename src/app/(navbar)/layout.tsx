54f47dbb9befd4178869064b44e5199449027b06
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2023-12-07 17:14:13 +0700

    feat: profile page

--- src/app/(navbar)/layout.tsx
+++ src/app/(navbar)/layout.tsx
@@ -3,8 +3,8 @@ import Navbar from "../components/navbar";
 const NavbarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full w-full flex flex-col relative">
-      <div className="flex-1 bg-[#F5F6F7] overflow-y-auto">
-        <div className="px-[5%] pt-10">{children}</div>
+      <div className="flex-1 bg-[#F5F6F7] overflow-y-auto h-full">
+        <div className="px-[5%] pt-10 h-full">{children}</div>
       </div>
       <Navbar />
     </div>
---
+++ src/app/(navbar)/profile/page.tsx
@@ -0,0 +1,47 @@
+import { HiTicket } from "react-icons/hi2";
+import { FaBell } from "react-icons/fa";
+import { HiInformationCircle } from "react-icons/hi";
+import Image from "next/image";
+import { IoLogOut } from "react-icons/io5";
+
+const Page = () => {
+  return (
+    <div className="w-full h-full flex flex-col pb-10">
+      <h1 className="font-semibold text-lg mb-5">Profile</h1>
+      <div className="w-full flex items-center gap-4">
+        <div className="bg-gray-300 w-20 aspect-square rounded-full relative overflow-hidden">
+          <Image
+            src={"profile/face.svg"}
+            fill={true}
+            alt="Profile"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <h1 className="font-semibold text-lg">Kevin Sebastian</h1>
+          <p className="text-primary-100">Edit Profile</p>
+        </div>
+      </div>
+      <div className="w-full flex flex-col mt-4 gap-3 flex-1">
+        <div className="w-full bg-gradient-to-b from-primary-100 to-primary-200 flex text-white items-center rounded-xl py-3 px-4 gap-2">
+          <HiTicket />
+          <p>My Membership</p>
+        </div>
+        <div className="w-full bg-gradient-to-b from-primary-100 to-primary-200 flex text-white items-center rounded-xl py-3 px-4 gap-2">
+          <FaBell />
+          <p>Notification</p>
+        </div>
+        <div className="w-full bg-gradient-to-b from-primary-100 to-primary-200 flex text-white items-center rounded-xl py-3 px-4 gap-2">
+          <HiInformationCircle />
+          <p>About</p>
+        </div>
+      </div>
+      <div className="mt-auto w-full bg-gradient-to-b from-red-600 to-red-500 flex text-white items-center rounded-xl py-3 px-4 gap-2">
+        <IoLogOut />
+        <p>Log Out</p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
